c258265f08a3823903f6fc96ac7e511a3d2496c6
Kanishk Mishra <108087143+Kanishk2805@users.noreply.github.com>
2026-01-12 01:41:57 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20 bg-gradient-to-t from-green-200 to-white dark:from-gray-600 dark:to-black dark:text-white">
       <img width={100} src={assets.logo} alt="" />
       <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden dark:text-white">
-        Copyrights 2024 &copy; AyushKumar | All rights reserved.
+        Copyrights 2024 &copy; Kanishk | All rights reserved.
       </p>
       <div className="flex gap-3">
         <img
